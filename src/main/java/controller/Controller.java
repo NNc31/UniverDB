9490abe128e484e885e5753b011046c01be5069a
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2021-04-10 18:14:28 +0300

    Частично написал контроллер, воркердао, добавил ApiWord

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,64 @@
 package controller;
 
+import model.Worker;
+import service.APIWord;
+import service.WorkerDAO;
+import view.AddMenu;
+import view.EditMenu;
+import view.FilterMenu;
+import view.MainMenu;
+
+import java.util.List;
+
 public class Controller {
+    private WorkerDAO workerDAO = new WorkerDAO();
+    private int minAge;
+    private int maxAge;
+    private String rank;
+
+    public void launchMain(){
+        MainMenu mainMenu = new MainMenu(this);
+        mainMenu.showContent();
+    }
+    public void launchAdd(){
+        AddMenu addMenu = new AddMenu(this);
+        addMenu.showContent();
+    }
+    public void launchEdit(){
+        EditMenu editMenu = new EditMenu(this);
+        editMenu.showContent();
+    }
+    public void launchDelete(){
+
+    }
+    public void launchFilter(){
+        FilterMenu filterMenu = new FilterMenu(this);
+        filterMenu.showContent();
+    }
+
+    public void addWorker(Worker worker){
+        workerDAO.create(worker);
+    }
+    public void editWorker(int id, Worker worker){ //добавил параметр Worker
+        workerDAO.update(id, worker);
+    }
+    public void deleteWorker(int id){
+        workerDAO.delete(id);
+    }
+
+    public List<Worker> filter(int minAge, int maxAge, String rank){
+        this.minAge = minAge;
+        this.maxAge = maxAge;
+        this.rank = rank;
+        return workerDAO.read(minAge, maxAge, rank);
+    }
+    public List<Worker> filter(){
+        return workerDAO.read(minAge, maxAge, rank);
+    }
+    public void saveWorkers(){
+        APIWord apiWord = new APIWord();
+        apiWord.writeToFile(filter());
+    }
+
 
 }
--- src/main/java/model/Worker.java
+++ src/main/java/model/Worker.java
@@ -3,7 +3,7 @@ package model;
 import java.time.LocalDate;
 
 public class Worker {
-
+    private String id;
     private String surname;
     private int age;
     private String department;
@@ -13,4 +13,88 @@ public class Worker {
     private String degree;
     private String rank;
 
+    public Worker(String id, String surname, int age, String department, LocalDate birthDate,
+                  LocalDate employmentDate, String position, String degree, String rank) {
+        this.id = id;
+        this.surname = surname;
+        this.age = age;
+        this.department = department;
+        this.birthDate = birthDate;
+        this.employmentDate = employmentDate;
+        this.position = position;
+        this.degree = degree;
+        this.rank = rank;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getEmploymentDate() {
+        return employmentDate;
+    }
+
+    public void setEmploymentDate(LocalDate employmentDate) {
+        this.employmentDate = employmentDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
 }
